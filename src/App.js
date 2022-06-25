d183005fa7b0cbf514092bb9de65ca5e0de29f57
Max Jeon <mjeon@Maxs-Air.carolina.rr.com>
2022-06-25 17:20:42 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState} from "react";
 
 function App() {
+  const CLIENT_ID = "f298e66445ac409abad93f3b2d61dc0c";
+  const REDIRECT_URI = "http://localhost:3000/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  const [token, setToken] = useState("");
+  const [searchKey, setSearchKey] = useState("");
+
+  useEffect( () => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+    if (!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+    setToken(token);
+
+  }, [])
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  }
+
+  async function searchArtists(e) {
+    e.preventDefault();
+    console.log(searchKey);
+    const data = await fetch(`https://api.spotify.com/v1/search?type=artist&q=${searchKey}`, {
+      method: "GET",
+      headers: {
+        "Authorization": 'Bearer ' + token,
+      }
+    })
+    const yeet = await data.json();
+    console.log(yeet);
+  }
+  // console.log(token)
+
+  async function getArtists(e) {
+    const data = await fetch(`https://api.spotify.com/v1/me/top/artists`, {
+      method: "GET",
+      headers: {
+        "Authorization": 'Bearer ' + token,
+      }
+    })
+    const yeet = await data.json();
+    console.log(yeet);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spotify React</h1>
+      {!token ? 
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`}>Login to Spotify</a>
+      : <button onClick={logout}>Logout</button>}
+
+      {token ? 
+        <form onSubmit={searchArtists}>
+          <input type="text" onChange={e => setSearchKey(e.target.value)}></input>
+          <button type="submit">Search</button>
+        </form>
+        : <h2>Please login</h2>
+      }
+      {token ? 
+          <button onClick={getArtists}>Get Top Artists</button>
+        : <h2>Please login</h2>
+      }
+
     </div>
   );
 }
