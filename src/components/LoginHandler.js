5116f5be04d8dee64e03b0935b07423cb0a26c75
minkmax <mjeon@Maxs-Air.carolina.rr.com>
2022-06-27 19:36:10 -0400

    updated url value so that it is dynamic instead of localhost

--- src/components/LoginHandler.js
+++ src/components/LoginHandler.js
@@ -2,7 +2,7 @@ import classes from "../css/LogButton.module.css";
 
 const LoginHandler = (props) => {
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const REDIRECT_URI = "http://localhost:3000/";
+  const REDIRECT_URI = window.location.origin;
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
