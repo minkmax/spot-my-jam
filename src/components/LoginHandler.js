486c8a7f4cba7492eb841d008f46bbda5c7b5bf5
minkmax <mjeon@Maxs-Air.carolina.rr.com>
2022-06-27 19:46:57 -0400

    proposed new fix based off location.href

--- src/components/LoginHandler.js
+++ src/components/LoginHandler.js
@@ -2,13 +2,13 @@ import classes from "../css/LogButton.module.css";
 
 const LoginHandler = (props) => {
   const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-  const REDIRECT_URI = window.location.origin;
+  const REDIRECT_URI = window.location.href;
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
   const RESPONSE_TYPE = "token";
 
-  console.log("yeet", window.location)
+  console.log("candy")
 
-  const loginLink = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}/&response_type=${RESPONSE_TYPE}&scope=user-top-read`;
+  const loginLink = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`;
 
   return (
     <a href={loginLink} className={classes["log-button"]}>
